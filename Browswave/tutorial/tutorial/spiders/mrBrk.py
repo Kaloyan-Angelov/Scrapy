4a72c45a7d35876a53f705df5c85f68bb2a79db4
Kaloyan-Angelov <work.kaloyanA@gmail.com>
2019-05-12 18:33:16 +0300

    price is only digits

--- Browswave/tutorial/tutorial/spiders/mrBrk.py
+++ Browswave/tutorial/tutorial/spiders/mrBrk.py
@@ -6,29 +6,28 @@ class MrbrkSpider(scrapy.Spider):
     name = 'mrBrk'
     allowed_domains = ['mr-bricolage.bg']
     start_urls = ['https://mr-bricolage.bg/bg/Instrumenti/Avto-i-veloaksesoari/Veloaksesoari/ZhILETKA-SVETLOOTRZhZELENA/p/857037']
-    # start_urls = []
-    # start_urls = response.css("div.product div.title a::attr(href)").getall()
-    # def start_requests(self):
-    #     urls = response.css("div.product div.title a::attr(href)").getall()
-    #     for url in urls:
-    #         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # urls = response.css("div.product div.title a::attr(href)").getall()
-        # for url in urls:
-        #     yield scrapy.Request(url=url, callback=self.parse)
-
+        price_to_filter = response.css("p.price::text").get()
+        price = []
+        for i in price_to_filter:
+            if ord(i) < 48 and ord(i) > 57 and ord(i) != 46:
+                pass
+            else:
+                price.append(i)
+        price = "".join(price)
+        price = price.strip().split()[0]
         yield {
             'image': response.css("section.product-single img::attr(src)").get(),
             'title': response.css("h1::text").get(),
-            'price': response.css("p.price::text").get(),
+            "price": price,
             'classifications': response.css("div.product-classifications td.attrib::text").getall()
         }
 
-        next_page = response.css("li.pagination-next a::attr(href)").get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        # next_page = response.css("li.pagination-next a::attr(href)").get()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
 
 # gets all from curr page products
 # response.css("div.product div.title a::attr(href)").getall()
