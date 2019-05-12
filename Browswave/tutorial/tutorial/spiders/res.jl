8174c5e7e5470d449ce7598c525b22f324ce596a
Kaloyan-Angelov <work.kaloyanA@gmail.com>
2019-05-13 00:48:18 +0300

    removed Browswave folder

--- Browswave/tutorial/tutorial/spiders/res.jl
+++
@@ -1,3 +0,0 @@
-{"image": "https://mr-bricolage.bg/medias/sys_master/products/hd1/h46/8809537863710.jpg", "title": "ЖИЛЕТКА СВЕТЛООТРЖ.ЗЕЛЕНА", "price": "5,29", "classifications": {"Произход": "КИТАЙ", "Наименование": "ЖИЛЕТКА СВЕТЛООТРЖ.ЗЕЛЕНА"}}
-{"image": "https://mr-bricolage.bg/medias/sys_master/products/hd1/h46/8809537863710.jpg", "title": "ЖИЛЕТКА СВЕТЛООТРЖ.ЗЕЛЕНА", "price": "5,29", "classifications": {"Произход": "КИТАЙ", "Наименование": "ЖИЛЕТКА СВЕТЛООТРЖ.ЗЕЛЕНА"}}
-{"image": "https://mr-bricolage.bg/medias/sys_master/products/hd1/h46/8809537863710.jpg", "title": "ЖИЛЕТКА СВЕТЛООТРЖ.ЗЕЛЕНА", "price": "5,29", "classifications": {"Произход": "КИТАЙ", "Наименование": "ЖИЛЕТКА СВЕТЛООТРЖ.ЗЕЛЕНА"}}
